1c6e11134d6213235a46bcf2edb74b35e81572e1
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-06-13 20:09:40 +0200

    Update for publishing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='reddit-wallpapers-macosx',
+  packages=['reddit-wallpapers-macosx'],
+  version='0.1',
+  description='Reddit Wallpapers for MacOSX',
+  author='Mario Lamacchia',
+  author_email='mariolamacchia@gmail.com',
+  url='https://github.com/mariolamacchia/reddit-wallpapers-macosx',
+  download_url='https://github.com/mariolamacchia/reddit-wallpapers-macosx/archive/0.1.tar.gz',
+  keywords=['wallpapers', 'macosx', 'reddit'],
+  classifiers=[],
+)
