53e04baaaaabdf2f40833ca3353b93b3082f6a32
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-06-18 17:51:20 +0200

    Update dmg background

--- build.sh
+++ build.sh
@@ -21,14 +21,14 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 1040, 500}
+           set the bounds of container window to {400, 100, 1020, 450}
            set theViewOptions to the icon view options of container window
            set arrangement of theViewOptions to not arranged
-           set icon size of theViewOptions to 100
+           set icon size of theViewOptions to 56
            set background picture of theViewOptions to file ".background:'${backgroundPictureName}'"
            make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-           set position of item "'${applicationName}'" of container window to {160, 190}
-           set position of item "Applications" of container window to {480, 190}
+           set position of item "'${applicationName}'" of container window to {110, 98}
+           set position of item "Applications" of container window to {110, 195}
            update without registering applications
            delay 5
            close
