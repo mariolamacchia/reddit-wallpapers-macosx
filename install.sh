7746beb5cb4ebfe03ef8f1b08f36736061d95066
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-06-18 16:16:57 +0200

    install.sh: install for every user

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 curl -L $(curl -s https://api.github.com/repos/mariolamacchia/reddit-wallpapers-macosx/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > reddit-wallpapers.dmg
 sudo hdiutil attach reddit-wallpapers.dmg
-cp -R "/Volumes/Reddit Wallpapers/Reddit Wallpapers.app" ~/Applications
+cp -R "/Volumes/Reddit Wallpapers/Reddit Wallpapers.app" /Applications
 sudo hdiutil detach "/Volumes/Reddit Wallpapers"
 rm reddit-wallpapers.dmg
-open ~/Applications/Reddit\ Wallpapers.app
+open /Applications/Reddit\ Wallpapers.app
