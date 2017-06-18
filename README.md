e50ef4a396aceb7f674f81a3f7315aebe66fa6d1
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-06-18 16:12:19 +0200

    Add instructions to install it from command line

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Bored of looking at the same wallpaper every single day?
 
 This script continuously changes wallpapers from your favourite subreddits. You can specify how often to change, and from which subreddits to take the wallpapers.
 
-[Download it here!](https://github.com/mariolamacchia/reddit-wallpapers-macosx/releases/latest)
+[Download it here!](https://github.com/mariolamacchia/reddit-wallpapers-macosx/releases/latest) or install it from command line:
+
+```sh
+curl https://raw.githubusercontent.com/mariolamacchia/reddit-wallpapers-macosx/master/install.sh | sh
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 curl -L $(curl -s https://api.github.com/repos/mariolamacchia/reddit-wallpapers-macosx/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > reddit-wallpapers.dmg
 sudo hdiutil attach reddit-wallpapers.dmg
-cp -R "/Volumes/reddit-wallpapers/Reddit Wallpapers.app" ~/Applications
-sudo hdiutil detach /Volumes/reddit-wallpapers
+cp -R "/Volumes/Reddit Wallpapers/Reddit Wallpapers.app" ~/Applications
+sudo hdiutil detach "/Volumes/Reddit Wallpapers"
 rm reddit-wallpapers.dmg
-open "~/Applications/Reddit Wallpapers.app"
+open ~/Applications/Reddit\ Wallpapers.app
